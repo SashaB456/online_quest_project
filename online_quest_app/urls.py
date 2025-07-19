1ae8de31275c4616a74f616de036b3d22afa8dfd
SashaB456 <ppppppppppppppdhdh@example.com>
2025-07-19 20:25:21 +0300

    Added a button for playing quizzes

--- online_quest_app/urls.py
+++ online_quest_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
-from .views import QuizList, QuizDetail, QuizCreate, QuizDelete
+from .views import QuizList, QuizDetail, QuizCreate, QuizDelete, Challenge, play_quiz, results
 urlpatterns = [
     path('quiz_list/', QuizList.as_view(), name='quiz-list'),
     path('quiz_detail/<int:pk>', QuizDetail.as_view(), name='quiz-detail'),
     path('quiz_create/', QuizCreate.as_view(), name='quiz-create'),
     path('quiz_delete/<int:pk>/', QuizDelete.as_view(), name='quiz-delete'),
+    path('challenge/<int:quiz_id>/', Challenge, name='challenge'),
+    path('play_quiz/<int:quiz_id>/<int:question_number>', play_quiz, name='play-quiz'),
+    path('results/<int:quiz_id>/', results, name='results'),
 ]
--- online_quest_app/views.py
+++ online_quest_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, DeleteView
 from .models import Quiz, Question
-from .forms import QuizForm, QuestionForm
+from .forms import QuizForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .mixins import UserIsOwnerMixin
 # Create your views here.
@@ -12,13 +12,6 @@ class QuizList(ListView):
     model = Quiz
     template_name = 'online_quest_app/quiz_list.html'
     context_object_name = 'quizzes'
-    def get(self, request):
-        super().get(request)
-        return render(request, self.template_name, context=self.get_context_data())
-class QuestionList(ListView):
-    model = Question
-    template_name = 'online_quest_app/question_list.html'
-    context_object_name = 'questions'
     def get(self, request):
         super().get(request)
         return render(request, self.template_name, context=self.get_context_data())
@@ -35,16 +28,44 @@ class QuizCreate(LoginRequiredMixin, CreateView):
         user = self.request.user
         form.instance.user = user
         return super().form_valid(form)
-class QuestionCreate(LoginRequiredMixin, CreateView):
-    model = Question
-    form_class = QuestionForm
-    template_name = 'online_quest_app/quiz_create.html'
-    success_url = reverse_lazy('quiz-list')
-    def form_valid(self, form):
-        user = self.request.user
-        form.instance.user = user
-        return super().form_valid(form)
 class QuizDelete(LoginRequiredMixin, DeleteView):
     model = Quiz
     template_name = 'online_quest_app/quiz_delete.html'
     success_url = reverse_lazy('quiz-list')
+def Challenge(request, quiz_id):
+    score = 0
+    quiz = Quiz.objects.get(id=quiz_id)
+    request.session['quiz_id'] = quiz.id
+    request.session['question_number'] = 1
+    request.session['score'] = score
+
+    return redirect('play-quiz', quiz_id=quiz_id, question_number=1)
+def play_quiz(request, quiz_id, question_number=1):
+    quiz = Quiz.objects.get(id=quiz_id)
+    question_number = request.session.get('question_number', 1)
+    questions = list(quiz.question.all())
+    quiz_size = len(questions)
+    current_question = questions[question_number - 1]
+    if request.method == "POST":
+        answer = request.POST.get('answer')
+        correct_answer = current_question.correct_answer
+        if answer.lower() == correct_answer.lower():
+            request.session['score'] += 1
+        if question_number < quiz_size:
+            request.session['question_number'] = question_number + 1
+            return redirect('play-quiz', quiz_id=quiz_id, question_number=request.session['question_number'])
+        else:
+            return redirect('results', quiz_id=quiz_id)
+    #answers = current_question
+    return render(request, 'online_quest_app/play_quiz.html', {
+        'quiz': quiz,
+        'question': current_question,
+        'question_number': question_number,
+        'score': request.session.get('score'),
+    })
+def results(request, quiz_id):
+    return render(request, 'online_quest_app/results.html', {
+        'score': request.session.get('score'),
+        'quiz': Quiz.objects.get(id=quiz_id),
+        'quiz_len': request.session.get('question_number')
+    })
